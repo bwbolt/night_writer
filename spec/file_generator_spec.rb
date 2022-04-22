c30c48c383b8497c15231e8eba3f80f453e4eaf1
Bryce Wein <bryce.wein@gmail.com>
2022-04-22 10:49:50 -0400

    Test: Test for writing to a new brail file

--- spec/file_generator_spec.rb
+++ spec/file_generator_spec.rb
@@ -15,14 +15,16 @@ describe FileGenerator do
 
   it 'can print the creation line' do
     night_writer = FileGenerator.new
-    expect(night_writer.print_message).to eq("Created '' containing 1048575 characters")
+    allow(night_writer).to receive(:incoming_file_length).and_return(1)
+    allow(night_writer).to receive(:outgoing_file_name).and_return('braille.txt')
+    expect(night_writer.print_message).to eq("Created 'braille.txt' containing 1 characters")
   end
 
   it 'can write to a brail.txt file' do
     night_writer = FileGenerator.new
-    allow(night_writer).to receive(:incoming_file).and_return('a')
-    allow(night_writer).to receive(:outgoing_file).and_return('english_to_english.txt')
-    night_writer.translate_to_brail
-    expect(File.read(@outgoing_file).chomp).to eq('a')
+    allow(night_writer).to receive(:translate_to_brail).and_return('a')
+    allow(night_writer).to receive(:outgoing_file_name).and_return('english_to_english.txt')
+    night_writer.write_to_new_brail_file
+    expect(File.read('english_to_english.txt').chomp).to eq('a')
   end
 end
