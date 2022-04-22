eb2ec2909a5885e1e9c57249e272ad6b3efbb9b8
Bryce Wein <bryce.wein@gmail.com>
2022-04-21 20:22:12 -0400

    Test: Add night object initial test

---
+++ spec/file_generator_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require './lib/file_generator
+
+describe FileGenerator do
+  it 'exists with attributes' do
+    night_writer = FileGenerator.new
+    expect(night_writer).to be_a FileGenerator
+  end
+end
