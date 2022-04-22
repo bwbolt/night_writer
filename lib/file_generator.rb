a467de774144841375b4eee125869e2204bb9bbb
Bryce Wein <bryce.wein@gmail.com>
2022-04-22 10:50:15 -0400

    Feat: write to new file

--- lib/file_generator.rb
+++ lib/file_generator.rb
@@ -2,11 +2,31 @@ class FileGenerator
   attr_reader :incoming_file, :outgoing_file
 
   def initialize
-    @incoming_file = File.read(ARGV[0]).chomp
+    @incoming_file = ARGV[0]
     @outgoing_file = ARGV[1]
   end
 
   def print_message
-    "Created '#{@outgoing_file}' containing #{@incoming_file.length} characters"
+    "Created '#{outgoing_file_name}' containing #{incoming_file_length} characters"
+  end
+
+  def read_incoming_file
+    File.read(@incoming_file).chomp
+  end
+
+  def incoming_file_length
+    read_incoming_file.length
+  end
+
+  def translate_to_brail
+    incoming_file.map { |letter| english_to_brail[letter] }
+  end
+
+  def outgoing_file_name
+    @outgoing_file
+  end
+
+  def write_to_new_brail_file
+    File.write(outgoing_file_name, translate_to_brail)
   end
 end
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,3 +2,4 @@ require './lib/file_generator'
 
 file_generator = FileGenerator.new
 p file_generator.print_message
+file_generator.translate_to_brail
