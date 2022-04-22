1cf67f69cca1a559e774cbe34f8872b0cc68b93c
Bryce Wein <bryce.wein@gmail.com>
2022-04-22 10:24:49 -0400

    Test: add write to file test for english to english

--- spec/file_generator_spec.rb
+++ spec/file_generator_spec.rb
@@ -7,14 +7,22 @@ describe FileGenerator do
   it 'exists with attributes' do
     night_writer = FileGenerator.new
     expect(night_writer).to be_a FileGenerator
-    allow(night_writer).to receive(:incoming_file).and_return('message.txt')
+    allow(night_writer).to receive(:incoming_file).and_return('a')
     allow(night_writer).to receive(:outgoing_file).and_return('braille.txt')
-    expect(night_writer.incoming_file).to eq('message.txt')
+    expect(night_writer.incoming_file).to eq('a')
     expect(night_writer.outgoing_file).to eq('braille.txt')
   end
 
   it 'can print the creation line' do
     night_writer = FileGenerator.new
     expect(night_writer.print_message).to eq("Created '' containing 1048575 characters")
+  end
+
+  it 'can write to a brail.txt file' do
+    night_writer = FileGenerator.new
+    allow(night_writer).to receive(:incoming_file).and_return('a')
+    allow(night_writer).to receive(:outgoing_file).and_return('english_to_english.txt')
+    night_writer.translate_to_brail
+    expect(File.read(@outgoing_file).chomp).to eq('a')
   end
 end
